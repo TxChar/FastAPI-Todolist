05a031351077d303d7ed8aa9c2c996877f0a24f5
TxChar <pontogreen1@gmail.com>
2025-03-07 10:19:56 +0700

    feat: 🎸 import task's route

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 import app.dependencies
-from app.routers.v1 import users
+from app.routers.v1 import users, tasks
 
 app = FastAPI()
 
 # รวม Routers
 app.include_router(users.router)
+app.include_router(tasks.router)
 
 
 @app.get("/")
