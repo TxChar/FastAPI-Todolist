3be70e8117642dd592c739dbf04257804ec47e22
TxChar <pontogreen1@gmail.com>
2025-03-07 10:19:20 +0700

    feat: 🎸 create task's api schema

---
+++ app/schemas/task.py
@@ -0,0 +1,38 @@
+from pydantic import BaseModel, Field
+from typing import List, Optional
+from datetime import datetime
+
+
+class SubTaskBase(BaseModel):
+    name: str
+    status: str = Field(default="Pending", pattern="^(Pending|In Progress|Done)$")
+    expected_date: Optional[datetime | None] = None
+
+
+class SubTaskCreate(SubTaskBase):
+    pass
+
+
+class SubTaskResponse(SubTaskBase):
+    id: str
+
+    class Config:
+        orm_mode = True
+
+
+class MainTaskBase(BaseModel):
+    name: str
+    status: str = Field(default="Pending", pattern="^(Pending|In Progress|Done)$")
+    expected_date: Optional[datetime] = None
+
+
+class MainTaskCreate(MainTaskBase):
+    sub_tasks: List[SubTaskCreate] = []
+
+
+class MainTaskResponse(MainTaskBase):
+    id: str
+    sub_tasks: List[SubTaskResponse] = []
+
+    class Config:
+        orm_mode = True
