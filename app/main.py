dfb8d0d916c8791df2f728a3cfe570d33af83ca7
TxChar <pontogreen1@gmail.com>
2025-03-07 15:20:41 +0700

    refactor: 💡 remove user's route and comment home's route

--- app/main.py
+++ app/main.py
@@ -1,16 +1,14 @@
 from fastapi import FastAPI
 import app.dependencies
 
-from app.routers.v1 import users, tasks, sub_tasks
+from app.routers.v1 import tasks, sub_tasks
 
 app = FastAPI()
 
-# รวม Routers
-app.include_router(users.router)
 app.include_router(tasks.router)
 app.include_router(sub_tasks.router)
 
 
-@app.get("/")
-def home():
-    return {"message": "Welcome to FastAPI with MongoDB!"}
+# @app.get("/")
+# def home():
+#     return {"message": "Welcome to FastAPI with MongoDB!"}
